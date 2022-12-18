874d594e009ea2a14956955ea1b458be90062175
kihaku565 <vas.matyushkin@yandex.ru>
2022-12-18 12:14:40 +0700

    1st review

--- scripts/index.js
+++ scripts/index.js
@@ -1,15 +1,14 @@
-let Popup = document.querySelector('.popup'); //окно редактирования профиля
-let formElement = document.querySelector('.popup__container'); //в целом можно было и удалить, не, т.к см. выше? 
-let nameInput = formElement.querySelector('#profile-name'); //ввод значения в поле с именем
-let jobInput = formElement.querySelector('#profile-about'); //ввод значения в поле "увлечения"
+let popup = document.querySelector('.popup'); //окно редактирования профиля
+let nameInput = popup.querySelector('#profile-name'); //ввод значения в поле с именем
+let jobInput = popup.querySelector('#profile-about'); //ввод значения в поле "увлечения"
 //кнопки
 let editProfileBtn = document.querySelector('.profile__edit-btn'); //кнорка редактирования профиля
-let saveProfileBtn = formElement.querySelector('.popup__save-btn'); //кнопка сохранения изменений профиля
-let closeProfileBtn = formElement.querySelector('.popup__close-btn'); //кнопка закрытия окна редактирования профиля
+let saveProfileBtn = popup.querySelector('.popup__save-btn'); //кнопка сохранения изменений профиля
+let closeProfileBtn = popup.querySelector('.popup__close-btn'); //кнопка закрытия окна редактирования профиля
 
 //открываем окно редактирования профиля
 function openEditProfile() {
-  Popup.classList.add('popup_opened'); //добавляет модификатор для открытия окна редактирования профиля
+  popup.classList.add('popup_opened'); //добавляет модификатор для открытия окна редактирования профиля
   nameInput.value = document.querySelector('.profile__name').textContent; //берет значение в имени профиля
   jobInput.value = document.querySelector('.profile__about').textContent; //берет значение с "увлечения"
 }
@@ -20,14 +19,14 @@ function handleFormSubmit(evt) {
   evt.preventDefault(); //Эта строчка отменяет стандартную отправку формы
   document.querySelector('.profile__name').textContent = nameInput.value; //заменяем значение в имени профиля
   document.querySelector('.profile__about').textContent = jobInput.value; //заменяем значение с "увлечения"
-  Popup.classList.remove('popup_opened'); //окно редактирования закрывается
+  popup.classList.remove('popup_opened'); //окно редактирования закрывается
 }
 //Прикрепляем обработчик к форме: он будет следить за событием “submit” - «отправка»
-formElement.addEventListener('submit', handleFormSubmit);
+popup.addEventListener('submit', handleFormSubmit);
 
 //закрываем окно редактирования профиля
 function closeEditProfile() {
-  Popup.classList.remove('popup_opened'); //окно редактирования закрывается
+  popup.classList.remove('popup_opened'); //окно редактирования закрывается
 }
 //клик по кнопке закрытия окна редактирования профиля
 closeProfileBtn.addEventListener('click', closeEditProfile);
